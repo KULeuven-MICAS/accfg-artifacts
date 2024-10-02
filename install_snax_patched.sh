f26d13333be79291eb1fc71c0686524dd168fddb
Josse Van Delm <josse.vandelm@gmail.com>
2024-10-02 13:35:48 +0200

    Add snax patch/installation script

---
+++ install_snax_patched.sh
@@ -0,0 +1,16 @@
+# Script that enters snax_cluster repository, and installs right version
+git clone https://github.com/KULeuven-MICAS/snax_cluster.git
+cd snax_cluster
+git checkout `cat ../commit`
+git submodule update --init 
+git apply ../increase_memory.patch
+make CFG_OVERRIDE=cfg/snax-streamer-gemm.hjson -C target/snitch_cluster rtl-gen
+make CFG_OVERRIDE=cfg/snax-streamer-gemm.hjson -C target/snitch_cluster bin/snitch_cluster.vlt -j$(nproc)
+make -B -C target/snitch_cluster sw CFG_OVERRIDE=cfg/snax-streamer-gemm.hjson SELECT_RUNTIME=rtl-generic SELECT_TOOLCHAIN=llvm-generic
+cp -r target/snitch_cluster/bin/snitch_cluster.vlt /opt/snax-streamer-gemm-rtl/bin/snitch_cluster.vl
+cp -r sw/snRuntime /opt/snax-streamer-gemm/sw/snRuntime
+cp -r target/snitch_cluster/sw /opt/snax-streamer-gemm/target/snitch_cluster/sw
+cp -r sw /opt/snax-streamer-gemm/sw
+
+
+
