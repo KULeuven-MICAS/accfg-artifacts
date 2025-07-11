f3862da2d668137833454d2be0aa7b43b16d02ec
Anton Lydike <me@antonlydike.de>
2025-07-11 12:01:07 +0100

    improve docker check

--- run-all.sh
+++ run-all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-if [[ ! -d /repo ]]; then
+if ! mountpoint /repo; then
 	docker run --rm -itv $PWD:/repo:z ghcr.io/kuleuven-micas/gemmini-test:latest /repo/run-all.sh || exit 1
 	exit 0
 fi
@@ -10,5 +10,4 @@ cd /repo/gemmini-rocc-tests && ./build.sh
 cd /repo/gemmini-rocc-tests/bareMetalMLIR && make all_binaries all_binaries_no_opt
 cd /repo && pip install . --break-system-packages
 python3 accfg_artifacts/gemmini_get_all_numbers.py gemmini-rocc-tests -o results.pkl
-
 
